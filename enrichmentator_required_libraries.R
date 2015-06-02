276fbff094174fbe595d7d1f1837b05d551fc899
guillermodeandajauregui <guillermodeandajauregui@gmail.com>
2015-06-02 16:08:00 -0500

    Added enrichmentator_required_libraries.R script, installs required packages for use of the file

---
+++ enrichmentator_required_libraries.R
@@ -0,0 +1,8 @@
+#Bioconductor needed libraries
+source("http://bioconductor.org/biocLite.R")
+biocLite("HTSanalyzeR")
+biocLite("org.Hs.eg.db")
+biocLite(KEGG.db)
+biocLite(HTSanalyzeR)
+#CRAN libraries needed
+install.packages("stringr")
